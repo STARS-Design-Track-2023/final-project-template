257b87d035de97b062c26318e963b417387e665b
1fahadaloufi <131143138+1fahadaloufi@users.noreply.github.com>
2023-07-06 10:06:53 -0400

    Update top_level_wrapper.sv
    edited reset sync

--- source/top_level_wrapper.sv
+++ source/top_level_wrapper.sv
@@ -8,15 +8,22 @@ module top_level_wrapper (
     input logic [33:0] gpio_in, // Breakout Board Pins
     output logic [33:0] gpio_out, // Breakout Board Pins
     output logic [33:0] gpio_oeb // Active Low Output Enable
-);
-
-    logic block_reset, block_reset_int, gated_reset;
-    assign gated_reset = ~ncs & nrst;
+); 
 
     // Reset Synchronizer
-    always_ff @ (posedge clk, negedge gated_reset) begin : RESET_SYNCHRONIZER
-        block_reset_int <= 1'b1;
-        block_reset <= block_reset_int;
+    logic ff1, ff2;
+    assign gated_reset = ~ncs & nrst;
+
+    always_ff @(posedge clk, negedge gated_reset) begin
+        if(~gated_reset) begin
+            ff1 <= 1'b0; 
+            ff2 <= 1'b0; 
+        end
+        else begin
+            ff1 <= 1'b1; 
+            ff2 <= ff1; 
+        end
+
     end
 
     design_module_name inst_name ();
